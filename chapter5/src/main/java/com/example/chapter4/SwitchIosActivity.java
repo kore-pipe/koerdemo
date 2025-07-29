fbf01a59ca55cba85767e58adf3366d59fb453b3
dongkoer <dongkeer@hosecloud.com>
2025-07-29 12:13:00 +0800

    练习8

--- chapter5/src/main/java/com/example/chapter4/SwitchIosActivity.java
+++ chapter5/src/main/java/com/example/chapter4/SwitchIosActivity.java
@@ -1,9 +1,8 @@
 package com.example.chapter4;
 
 import android.os.Bundle;
-import android.view.View;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,23 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SwitchDefaultActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
-    private TextView tv_result;
+public class SwitchIosActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    private TextView tv_result2 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_switch_default);
-        Switch sw_status = findViewById(R.id.sw_status);
-        tv_result = findViewById(R.id.tv_result);
-        sw_status.setOnCheckedChangeListener(this);
+        setContentView(R.layout.activity_switch_ios);
+       CheckBox ck_status =  findViewById(R.id.ck_status);
+       tv_result2 = findViewById(R.id.tv_result2);
+       ck_status.setOnCheckedChangeListener(this);
+
+
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         String desc = String.format("你 %s 了这个switch",isChecked ? "打开" : "关闭");
-        tv_result.setText(desc);
-
+        tv_result2.setText(desc);
 
     }
 }
