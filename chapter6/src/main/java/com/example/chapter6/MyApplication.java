375e969a6b94e3d0da09f6c501a8b7ac7aa37a8b
dongkoer <dongkeer@hosecloud.com>
2025-07-30 15:30:16 +0800

    prictice 13

--- chapter6/src/main/java/com/example/chapter6/MyApplication.java
+++ chapter6/src/main/java/com/example/chapter6/MyApplication.java
@@ -5,6 +5,10 @@ import android.content.res.Configuration;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.room.Room;
+
+import com.example.chapter6.Dao.BookDao;
+import com.example.chapter6.database.BookDataBase;
 
 import java.util.HashMap;
 
@@ -15,12 +19,32 @@ public class MyApplication extends Application {
     public static  MyApplication getInstance(){
         return mApp;
     }
+
+    //声明一个数据库对象
+    private BookDataBase bookDatabase;
+
+
+
    //在app启动的时候调用
     @Override
     public void onCreate() {
         super.onCreate();
         Log.d("dong","MyApplication onCreate");
+        mApp = this;
+
+
+        bookDatabase = Room.databaseBuilder(this,BookDataBase.class,"book")
+                //如果没有这个参数，发生数据库变更时，默认删除原数据库再新建数据库
+                .addMigrations()
+                .allowMainThreadQueries()
+                .build();
     }
+    public BookDataBase getBookDB(){
+        return bookDatabase;
+    }
+
+
+
     //终止的时候调用
     @Override
     public void onTerminate() {
---
+++ chapter6/src/main/java/com/example/chapter6/RoomWriteActivity2.java
@@ -0,0 +1,82 @@
+package com.example.chapter6;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chapter6.Dao.BookDao;
+import com.example.chapter6.entity.BookInfo;
+import com.example.chapter6.util.ToastUtil;
+
+import java.util.List;
+
+public class RoomWriteActivity2 extends AppCompatActivity implements View.OnClickListener {
+    private EditText et_name;
+    private EditText et_price;
+    private EditText et_author;
+    private EditText et_publish;
+
+    private BookDao bookDao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_room_write2);
+
+        et_name = findViewById(R.id.et_name);
+        et_price = findViewById(R.id.et_price);
+        et_author = findViewById(R.id.et_author);
+        et_publish = findViewById(R.id.et_publish);
+
+        findViewById(R.id.bt_save).setOnClickListener(this);
+        findViewById(R.id.bt_search).setOnClickListener(this);
+        findViewById(R.id.bt_delete).setOnClickListener(this);
+        findViewById(R.id.bt_update).setOnClickListener(this);
+
+        bookDao = MyApplication.getInstance().getBookDB().bookDao();
+    }
+
+    @Override
+    public void onClick(View v) {
+        String price = et_price.getText().toString();
+        String publish = et_publish.getText().toString();
+        String author = et_author.getText().toString();
+        String name = et_name.getText().toString();
+
+        switch (v.getId()) {
+            case R.id.bt_save:
+                BookInfo b1 = new BookInfo();
+                b1.setAuthor(author);
+                b1.setName(name);
+                b1.setPublish(publish);
+                b1.setPrice(Double.parseDouble(price));
+                bookDao.insert(b1);
+                ToastUtil.show(this,"保存成功！");
+                break;
+            case R.id.bt_search:
+                List<BookInfo> list = bookDao.queryAll();
+                for (BookInfo b : list) {
+                    Log.d("dong",b.toString());
+
+                }
+
+                break;
+            case R.id.bt_delete:
+
+                bookDao.delete();
+                break;
+            case R.id.bt_update:
+
+                break;
+        }
+    }
+}
